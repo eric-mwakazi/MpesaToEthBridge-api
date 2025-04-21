fbe7ea29768ae94699a3cf443261d52bba9ae21e
neivany1 <nevilenaivanny@gmail.com>
2025-04-21 07:34:29 +0300

    deployed the contract

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bridgeRoutes = require("./routes/bridge");
 const { swaggerUi, swaggerSpec } = require("./swagger/swagger-ui");
 const cors = require('cors');
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.1.0/swagger-ui.min.css';
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -11,7 +12,8 @@ app.get('/', (req, res) => {
     res.redirect('/api-docs');
   });
 app.use("/api", bridgeRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }))
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
