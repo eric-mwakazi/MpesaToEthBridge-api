cd5a26097f6122cb297e79b75813e6149c744fd4
neivany1 <nevilenaivanny@gmail.com>
2025-04-25 18:02:40 +0300

    added swagger to vercel

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const bridgeRoutes = require("./routes/bridge");
 const { swaggerSpec } = require("./swagger/swagger-ui");
 const swaggerUi = require("swagger-ui-express");
@@ -10,6 +12,9 @@ const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger
 app.use(cors());
 app.use(express.json());
 
+// Serve static files for Swagger UI
+app.use("/swagger-ui", express.static(path.join(__dirname, "node_modules", "swagger-ui-dist")));
+
 // Home redirect
 app.get("/", (req, res) => {
   res.redirect("/api-docs");
@@ -17,7 +22,12 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api", bridgeRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCssUrl: CSS_URL,
+  swaggerUrl: "/swagger-ui/swagger-ui-bundle.js"
+}));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));    
+app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
+
 module.exports = app;
